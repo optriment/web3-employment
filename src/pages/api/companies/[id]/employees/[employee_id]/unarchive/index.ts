052da93bfe15ebe2fd916866bf6bfb8a405e5f36
Aditya Ghadge <adityaghadge360@gmail.com>
2023-05-12 00:58:05 +0530

    Create api endpoint for unarchiving employees

---
+++ src/pages/api/companies/[id]/employees/[employee_id]/unarchive/index.ts
@@ -0,0 +1,71 @@
+import { Prisma } from '@prisma/client'
+import * as Sentry from '@sentry/nextjs'
+import {
+  DATABASE_ERROR,
+  METHOD_NOT_ALLOWED,
+  UNHANDLED_ERROR,
+} from '@/lib/messages'
+import type { ApiResponse } from '@/lib/types/api'
+import { unarchiveEmployee } from '@/useCases/unarchiveEmployee'
+import type { Employee } from '@prisma/client'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export type EmployeeUnarchiveApiResponse = ApiResponse<Employee>
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  switch (req.method) {
+    case 'POST':
+      return await handlePOST(req, res)
+
+    default:
+      res.status(405)
+      res.json({ success: false, ...METHOD_NOT_ALLOWED })
+      return res.end()
+  }
+}
+
+const handlePOST = async (
+  req: NextApiRequest,
+  res: NextApiResponse<EmployeeUnarchiveApiResponse>
+) => {
+  try {
+    const useCase = await unarchiveEmployee(
+      req.query.id as string,
+      req.query.employee_id as string
+    )
+
+    res.status(useCase.status)
+
+    if (useCase.success) {
+      res.json({ success: true, data: useCase.data })
+    } else {
+      res.json({
+        success: false,
+        message: useCase.message,
+      })
+    }
+
+    return res.end()
+  } catch (e) {
+    Sentry.captureException(e)
+
+    res.status(500)
+
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      switch (e.code) {
+        case 'P2023': {
+          res.json({ success: false, message: 'Invalid UUID' })
+          break
+        }
+        default:
+          res.json({ success: false, ...DATABASE_ERROR })
+      }
+    } else {
+      res.json({ success: false, ...UNHANDLED_ERROR })
+    }
+
+    return res.end()
+  }
+}
+
+export default handler
