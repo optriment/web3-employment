3201f7bf4335bd9dbfa5fb1d5843563bc94cc890
Alexander Kadyrov <gruz0.mail@gmail.com>
2023-05-25 00:25:43 +0400

    Add signout menu item

--- src/layouts/user/components/menu.tsx
+++ src/layouts/user/components/menu.tsx
@@ -1,6 +1,6 @@
 import { WalletActionButton } from '@tronweb3/tronwallet-adapter-react-ui'
 import React from 'react'
-import { Menu } from 'semantic-ui-react'
+import { Button, Menu } from 'semantic-ui-react'
 
 const Component = () => (
   <Menu stackable size="huge">
@@ -10,6 +10,14 @@ const Component = () => (
     <Menu.Menu position="right">
       <Menu.Item>
         <WalletActionButton />
+      </Menu.Item>
+      <Menu.Item>
+        <Button
+          as="a"
+          href="/api/auth/signout"
+          icon="sign out"
+          content="Sign out"
+        />
       </Menu.Item>
     </Menu.Menu>
   </Menu>
