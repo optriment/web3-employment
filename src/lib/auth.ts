34f004234fbc718fe57173f96526550381101e22
Alexander Kadyrov <gruz0.mail@gmail.com>
2023-06-01 12:45:43 +0400

    Render auth providers conditionally

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,21 +4,21 @@ import GitHubProvider from 'next-auth/providers/github'
 import GoogleProvider from 'next-auth/providers/google'
 import { prisma } from '@/lib/prisma'
 import type { NextAuthOptions } from 'next-auth'
+import type { Provider } from 'next-auth/providers'
 
-export const authOptions: NextAuthOptions = {
-  debug: process.env.NODE_ENV === 'development',
-  secret: process.env.NEXTAUTH_SECRET,
-  adapter: PrismaAdapter(prisma),
-  session: {
-    strategy: 'database', // Store sessions in the database and store a sessionToken in the cookie for lookups
-    maxAge: 30 * 24 * 60 * 60, // 30 days to session expiry
-    updateAge: 24 * 60 * 60, // 24 hours to update session data into database
-  },
-  providers: [
+const providers: Provider[] = []
+
+if (process.env.GITHUB_CLIENT_ID) {
+  providers.push(
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    }),
+    })
+  )
+}
+
+if (process.env.GOOGLE_CLIENT_ID) {
+  providers.push(
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
@@ -29,19 +29,34 @@ export const authOptions: NextAuthOptions = {
           response_type: 'code',
         },
       },
-    }),
-    EmailProvider({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD,
-        },
+    })
+  )
+}
+
+providers.push(
+  EmailProvider({
+    server: {
+      host: process.env.EMAIL_SERVER_HOST,
+      port: process.env.EMAIL_SERVER_PORT,
+      auth: {
+        user: process.env.EMAIL_SERVER_USER,
+        pass: process.env.EMAIL_SERVER_PASSWORD,
       },
-      from: process.env.EMAIL_FROM,
-    }),
-  ],
+    },
+    from: process.env.EMAIL_FROM,
+  })
+)
+
+export const authOptions: NextAuthOptions = {
+  debug: process.env.NODE_ENV === 'development',
+  secret: process.env.NEXTAUTH_SECRET,
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: 'database', // Store sessions in the database and store a sessionToken in the cookie for lookups
+    maxAge: 30 * 24 * 60 * 60, // 30 days to session expiry
+    updateAge: 24 * 60 * 60, // 24 hours to update session data into database
+  },
+  providers: providers,
   callbacks: {
     async session({ session, user }) {
       session.userId = user.id
