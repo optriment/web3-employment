c88c5285fbf2d0fa6343e9d063f90268775ef222
Alexander Kadyrov <gruz0.mail@gmail.com>
2023-06-02 14:25:56 +0400

    Add link to privacy policy

--- src/layouts/landing/landing.tsx
+++ src/layouts/landing/landing.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Grid } from 'semantic-ui-react'
+import { Footer } from './components'
 
 type Props = {
   isMobile: boolean
@@ -9,6 +10,10 @@ type Props = {
 const Layout = ({ isMobile, children }: Props) => (
   <Grid container={!isMobile} columns={1} padded="vertically">
     <Grid.Column>{children}</Grid.Column>
+
+    <Grid.Column>
+      <Footer />
+    </Grid.Column>
   </Grid>
 )
 
