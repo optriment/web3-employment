2357ed19388da2b064b85f49e3828fe03891fb5b
Alexander Kadyrov <gruz0.mail@gmail.com>
2023-06-07 16:04:11 +0400

    Add privacy and cookie policies

---
+++ src/pages/policy/index.tsx
@@ -0,0 +1,43 @@
+import { useSession } from 'next-auth/react'
+import React from 'react'
+import { useHasMounted } from '@/hooks/use-has-mounted'
+import { LandingLayout, UserLayout } from '@/layouts'
+import { PrivacyPolicyScreen } from '@/screens/policy'
+import { getIsSsrMobile } from '@/utils/get-is-ssr-mobile'
+import { useIsMobile } from '@/utils/use-is-mobile'
+import type { GetServerSidePropsContext } from 'next'
+
+const Page: React.FC = () => {
+  const hasMounted = useHasMounted()
+  const isMobile = useIsMobile()
+
+  const { data: session } = useSession()
+
+  if (!hasMounted) {
+    return <p>Not mounted yet</p>
+  }
+
+  if (!session) {
+    return (
+      <LandingLayout isMobile={isMobile}>
+        <PrivacyPolicyScreen />
+      </LandingLayout>
+    )
+  }
+
+  return (
+    <UserLayout isMobile={isMobile}>
+      <PrivacyPolicyScreen />
+    </UserLayout>
+  )
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  return {
+    props: {
+      isSsrMobile: getIsSsrMobile(context),
+    },
+  }
+}
+
+export default Page
