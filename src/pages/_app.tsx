88e2887936d4aa7d520adf6231e934b179bd97c1
Alexander Kadyrov <gruz0.mail@gmail.com>
2023-06-23 11:59:55 +0400

    Move tracking codes into _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import { WalletProvider } from '@tronweb3/tronwallet-adapter-react-hooks'
 import { WalletModalProvider } from '@tronweb3/tronwallet-adapter-react-ui'
 import { TronLinkAdapter } from '@tronweb3/tronwallet-adapters'
 import { useRouter } from 'next/router'
+import Script from 'next/script'
 import { SessionProvider } from 'next-auth/react'
 import React, { useCallback, useMemo } from 'react'
 import Web3Provider from '@/context/web3-context'
@@ -16,6 +17,10 @@ import type { Session } from 'next-auth'
 
 import 'semantic-ui-css/semantic.min.css'
 import '@tronweb3/tronwallet-adapter-react-ui/style.css'
+
+const GOOGLE_ANALYTICS_ID = process.env.NEXT_PUBLIC_GA_ID
+const LINKEDIN_TRACKING_PARTNER_ID =
+  process.env.NEXT_PUBLIC_LINKEDIN_TRACKING_PARTNER_ID
 
 export default function App({
   Component,
@@ -48,16 +53,58 @@ export default function App({
   }, [])
 
   return (
-    <SessionProvider session={session}>
-      <WalletProvider onError={onError} adapters={adapters}>
-        <WalletModalProvider>
-          <Web3Provider>
-            <IsSsrMobileContext.Provider value={pageProps.isSsrMobile}>
-              <Component key={router.asPath} {...pageProps} />
-            </IsSsrMobileContext.Provider>
-          </Web3Provider>
-        </WalletModalProvider>
-      </WalletProvider>
-    </SessionProvider>
+    <>
+      {/* Google Analytics */}
+
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}
+        strategy="afterInteractive"
+      />
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${GOOGLE_ANALYTICS_ID}', { page_path: window.location.pathname });
+        `}
+      </Script>
+
+      {/* LinkedIn */}
+
+      <Script id="linkedin-vars" strategy="afterInteractive">
+        {`
+          _linkedin_partner_id = "${LINKEDIN_TRACKING_PARTNER_ID}";
+          window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+          window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+        `}
+      </Script>
+
+      <Script id="linkedin-tracking-code" strategy="afterInteractive">
+        {`
+          (function(l) {
+          if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+          window.lintrk.q=[]}
+          var s = document.getElementsByTagName("script")[0];
+          var b = document.createElement("script");
+          b.type = "text/javascript";b.async = true;
+          b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+          s.parentNode.insertBefore(b, s);})(window.lintrk);
+        `}
+      </Script>
+
+      <SessionProvider session={session}>
+        <WalletProvider onError={onError} adapters={adapters}>
+          <WalletModalProvider>
+            <Web3Provider>
+              <IsSsrMobileContext.Provider value={pageProps.isSsrMobile}>
+                <Component key={router.asPath} {...pageProps} />
+              </IsSsrMobileContext.Provider>
+            </Web3Provider>
+          </WalletModalProvider>
+        </WalletProvider>
+      </SessionProvider>
+    </>
   )
 }
